0138f2ecae949f17eda8a7b87ebc0dfc39a584af
Ahmed <a.hennawy97@hotmail.com>
2024-11-15 16:51:15 +0300

    Initializing App

---
+++ src/VendingMachine.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function VendingMachine() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default VendingMachine;
